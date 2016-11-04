cd41090223bd89bc9ea915f7f1fb51e2bc57155c
gagandeeprana <gagandeeprana@hotmail.com>
2016-11-04 21:34:22 +0530

    Changes Done

--- JIQA/src/main/java/com/jiqa/entity/QuestionBean.java
+++ JIQA/src/main/java/com/jiqa/entity/QuestionBean.java
@@ -3,16 +3,12 @@ package com.jiqa.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "questionmaster")
