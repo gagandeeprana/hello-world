078efee0b32733fc36f504188e4bdd870e20d0ef
Anuj Nayyar <anuj1307.nayyar@gmail.com>
2016-11-06 20:38:18 +0530

    Sample commit.

--- dbscript_dpu.sql
+++ dbscript_dpu.sql
@@ -18,7 +18,9 @@
 --
 -- Table structure for table `acipassengermaster`
 --
-
+DROP DATABASE IF EXISTS dpu;
+CREATE DATABASE dpu;
+use dpu;
 DROP TABLE IF EXISTS `acipassengermaster`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
